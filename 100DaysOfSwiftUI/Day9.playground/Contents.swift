e7aa27c0c17f9cd0bd27fa56742706cf8bedc079
yongfong <yongfongzhang@163.com>
2024-01-31 12:55:47 +0800

    checkpoint5

--- 100DaysOfSwiftUI/Day9.playground/Contents.swift
+++ 100DaysOfSwiftUI/Day9.playground/Contents.swift
@@ -9,3 +9,9 @@ func clean(tasks: () -> Void) {
 clean {
     print("clean table")
 }
+
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+luckyNumbers.filter{$0 % 2 != 0}.sorted{$0 < $1}.map{"\($0) is a lucky number"}.forEach {print($0)}
+
+
