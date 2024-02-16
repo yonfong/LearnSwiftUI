eb17dcb15ddb0537be521120846ffb5f5cea3536
yongfong <yongfongzhang@163.com>
2024-02-16 09:55:06 +0800

    learn day18

--- 100DaysOfSwiftUI/WebSplit/WebSplit/ContentView.swift
+++ 100DaysOfSwiftUI/WebSplit/WebSplit/ContentView.swift
@@ -17,13 +17,18 @@ struct ContentView: View {
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
-    var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople + 2)
+    var totalAmountCheck: Double {
         let tipSelection = Double(tipPercentage)
         
         let tipValue = checkAmount / 100 * tipSelection
         let grandTotal = checkAmount + tipValue
-        let amountPerPerson = grandTotal / peopleCount
+        
+        return grandTotal
+    }
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let amountPerPerson = totalAmountCheck / peopleCount
         
         return amountPerPerson
     }
@@ -45,16 +50,23 @@ struct ContentView: View {
                 
                 Section("How much tip do you want to leave?") {
                     Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
+                        ForEach(0..<101) {
                             Text($0, format: .percent)
                         }
                     }
-                    .pickerStyle(.segmented)
+                    .pickerStyle(.navigationLink)
                 }
+                .textCase(nil)
                 
-                Section {
+                Section("Total amount for the check") {
+                    Text(totalAmountCheck, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                .textCase(nil)
+                
+                Section("Amount per person") {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
+                .textCase(nil)
             }
             .navigationTitle("WeSplit")
             .toolbar {
