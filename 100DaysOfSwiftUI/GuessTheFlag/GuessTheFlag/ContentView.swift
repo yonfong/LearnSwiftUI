e9297488a047f798b11da63c05e37e7ffe4dbaa0
yongfong <yongfongzhang@163.com>
2024-02-19 16:05:31 +0800

    day21 showing the player`s score with an alert

--- 100DaysOfSwiftUI/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ 100DaysOfSwiftUI/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
     
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
     
-    @State private var showingAlert = false
     var body: some View {
         ZStack {
             Color.blue
@@ -26,18 +27,32 @@ struct ContentView: View {
                 
                 ForEach(0..<3) {number in
                     Button {
-                        
+                        flagTapped(number)
                     } label: {
                         Image(countries[number])
                     }
                 }
             }
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is ??")
+        }
     }
     
-    func executeDelete() {
-        print("Now deleting")
-        showingAlert = true
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
