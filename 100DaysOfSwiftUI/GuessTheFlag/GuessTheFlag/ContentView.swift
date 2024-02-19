234f7d4cf4cdfd06cd13dbb7392d6cd2a39a268d
yongfong <yongfongzhang@163.com>
2024-02-19 11:29:15 +0800

    day20 buttons and images

--- 100DaysOfSwiftUI/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ 100DaysOfSwiftUI/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,28 +9,68 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        ZStack {
-//            VStack(spacing: 0) {
-//                Color.red
-//                Color.blue
-//            }
-//            Text("Your content")
-//                .foregroundStyle(.secondary)
-//                .padding(50)
-//                .background(.ultraThinMaterial)
-//        }
-//        .ignoresSafeArea()
         VStack {
-            LinearGradient(stops: [
-                Gradient.Stop(color: .white, location: 0.45),
-                Gradient.Stop(color: .black, location: 0.55)], startPoint: .top, endPoint: .bottom)
-            RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
-            AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .red], center: .center)
-            Text("Your content")
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .foregroundStyle(.white)
-                .background(.red.gradient)
+            VStack(spacing: 20) {
+                Button("Delete Selection", action: executeDelete)
+                
+                Button("Delete Selection", role: .destructive, action: executeDelete)
+            }
+            
+            VStack {
+                Button("Button 1"){}
+                    .buttonStyle(.bordered)
+                Button("Button 2", role: .destructive) {}
+                    .buttonStyle(.bordered)
+                Button("Button 3"){}
+                    .buttonStyle(.borderedProminent)
+                Button("Button 2", role: .destructive) {}
+                    .buttonStyle(.borderedProminent)
+                    .tint(.mint)
+                Button {
+                    print("Buttons was tapped")
+                } label: {
+                    Text("Tap me!")
+                        .padding()
+                        .foregroundStyle(.white)
+                        .background(.red)
+                }
+                
+                Button {
+                    print("Edit button was tapped")
+                } label: {
+                    Image(systemName: "pencil")
+                        .padding()
+                        .background(.red)
+                }
+                Button("Edit", systemImage: "pencil") {
+                    print("Edit button taped")
+                }
+                Button {
+                    print("Edit button was tapped")
+                } label: {
+                    Label("Edit", systemImage: "pencil")
+                        .padding()
+                        .foregroundStyle(.white)
+                        .background(.red)
+                }
+                
+                Button {
+                    print("test")
+                } label: {
+                    HStack {
+                        Text("Edit")
+                        Image(systemName: "pencil")
+                    }
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.red)
+                }
+            }
         }
+    }
+    
+    func executeDelete() {
+        print("Now deleting")
     }
 }
 
