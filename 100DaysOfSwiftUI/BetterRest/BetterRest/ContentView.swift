58218e8a77ce1df0a8e0f0baa1c890d0a8ca459a
yongfong <yongfongzhang@163.com>
2024-02-22 15:58:57 +0800

    day26 datePicker

--- 100DaysOfSwiftUI/BetterRest/BetterRest/ContentView.swift
+++ 100DaysOfSwiftUI/BetterRest/BetterRest/ContentView.swift
@@ -10,12 +10,20 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var sleepCount = 8.0
+    @State private var wakeUp = Date.now
     
     var body: some View {
         VStack {
             Stepper("\(sleepCount.formatted()) hours", value: $sleepCount, in: 4...12, step: 0.25)
+            DatePicker("Please enter a date", selection: $wakeUp, in: Date.now..., displayedComponents: .hourAndMinute)
+                .labelsHidden()
         }
         .padding()
+    }
+    
+    func exampleDates() {
+        let tomorrow = Date.now.addingTimeInterval(86400)
+        let range = Date.now...tomorrow
     }
 }
 
