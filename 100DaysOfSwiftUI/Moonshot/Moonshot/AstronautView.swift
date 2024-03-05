d6ab851bd418cecc1cb00760608cc76c6dbf90aa
yongfong <yongfongzhang@163.com>
2024-03-05 14:40:38 +0800

    day41 finishing up with one last view

---
+++ 100DaysOfSwiftUI/Moonshot/Moonshot/AstronautView.swift
@@ -0,0 +1,34 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by sky on 2024/3/5.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    return AstronautView(astronaut: astronauts["aldrin"]!)
+        .preferredColorScheme(.dark)
+}
--- 100DaysOfSwiftUI/Moonshot/Moonshot/MissionView.swift
+++ 100DaysOfSwiftUI/Moonshot/Moonshot/MissionView.swift
@@ -8,13 +8,12 @@
 import SwiftUI
 
 struct MissionView: View {
-    let mission: Mission
-    
     struct CrewMember {
         let role: String
         let astronaut: Astronaut
     }
     
+    let mission: Mission
     let crew:[CrewMember]
     
     init(mission: Mission, astronauts: [String: Astronaut]) {
@@ -62,7 +61,7 @@ struct MissionView: View {
                     HStack {
                         ForEach(crew, id: \.role) { crewMember in
                             NavigationLink {
-                                Text("Astronaut details")
+                                AstronautView(astronaut: crewMember.astronaut)
                             } label: {
                                 HStack {
                                     Image(crewMember.astronaut.id)
