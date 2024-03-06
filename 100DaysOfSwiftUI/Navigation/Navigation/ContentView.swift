e02fa9d0fd22862202ef61600630906be486ed6a
yongfong <yongfongzhang@163.com>
2024-03-06 18:32:40 +0800

    day44 how to save navigationstack paths using codable

--- 100DaysOfSwiftUI/Navigation/Navigation/ContentView.swift
+++ 100DaysOfSwiftUI/Navigation/Navigation/ContentView.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var path = NavigationPath()
+    @State private var pathStore = PathStore()
     
     var body: some View {
-        NavigationStack(path: $path) {
+        NavigationStack(path: $pathStore.path) {
 //            List {
 //                ForEach(0..<5) { i in
 //                    NavigationLink("Select number \(i)", value: i)
@@ -21,20 +21,20 @@ struct ContentView: View {
 //                    NavigationLink("Select string \(i)", value: String(i))
 //                }
 //            }
-            DetailView(number: 0, path: $path)
+            DetailView(number: 0, path: $pathStore.path)
             .navigationDestination(for: Int.self) { selection in
-                DetailView(number: selection, path: $path)
+                DetailView(number: selection, path: $pathStore.path)
             }
             .navigationDestination(for: String.self) { selection in
                 Text("You selected string \(selection)")
             }
             .toolbar {
                 Button("push 565") {
-                    path.append(565)
+                    pathStore.path.append(565)
                 }
                 
                 Button("push hello") {
-                    path.append("hello")
+                    pathStore.path.append("hello")
                 }
             }
         }
---
+++ 100DaysOfSwiftUI/Navigation/Navigation/PathStore.swift
@@ -0,0 +1,40 @@
+//
+//  PathStore.swift
+//  Navigation
+//
+//  Created by sky on 2024/3/6.
+//
+
+import SwiftUI
+import Observation
+
+@Observable
+class PathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+        path = NavigationPath()
+    }
+    
+    func save() {
+        guard let representation = path.codable else { return }
+        do {
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
