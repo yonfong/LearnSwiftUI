be8623c19d43ccaf5c6da15cb211e0c1a1842ec0
yongfong <yongfongzhang@163.com>
2024-03-06 21:43:12 +0800

    day45 customizing the navigation bar appearance

--- 100DaysOfSwiftUI/Navigation/Navigation/ContentView.swift
+++ 100DaysOfSwiftUI/Navigation/Navigation/ContentView.swift
@@ -11,33 +11,45 @@ struct ContentView: View {
     @State private var pathStore = PathStore()
     
     var body: some View {
-        NavigationStack(path: $pathStore.path) {
-//            List {
-//                ForEach(0..<5) { i in
-//                    NavigationLink("Select number \(i)", value: i)
+        NavigationStack {
+            List(0..<100) { index in
+                Text("Row \(index)")
+            }
+            .navigationTitle("Title goes here")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(.blue)
+            .toolbarColorScheme(.dark)
+            .toolbar(.hidden, for: .navigationBar)
+        }
+        
+        
+//        NavigationStack(path: $pathStore.path) {
+////            List {
+////                ForEach(0..<5) { i in
+////                    NavigationLink("Select number \(i)", value: i)
+////                }
+////                
+////                ForEach(0..<5) { i in
+////                    NavigationLink("Select string \(i)", value: String(i))
+////                }
+////            }
+//            DetailView(number: 0, path: $pathStore.path)
+//            .navigationDestination(for: Int.self) { selection in
+//                DetailView(number: selection, path: $pathStore.path)
+//            }
+//            .navigationDestination(for: String.self) { selection in
+//                Text("You selected string \(selection)")
+//            }
+//            .toolbar {
+//                Button("push 565") {
+//                    pathStore.path.append(565)
 //                }
 //                
-//                ForEach(0..<5) { i in
-//                    NavigationLink("Select string \(i)", value: String(i))
+//                Button("push hello") {
+//                    pathStore.path.append("hello")
 //                }
 //            }
-            DetailView(number: 0, path: $pathStore.path)
-            .navigationDestination(for: Int.self) { selection in
-                DetailView(number: selection, path: $pathStore.path)
-            }
-            .navigationDestination(for: String.self) { selection in
-                Text("You selected string \(selection)")
-            }
-            .toolbar {
-                Button("push 565") {
-                    pathStore.path.append(565)
-                }
-                
-                Button("push hello") {
-                    pathStore.path.append("hello")
-                }
-            }
-        }
+//        }
 //        NavigationStack {
 //            List(0..<100) { i in
 //                NavigationLink("Select \(i)", value: i)
