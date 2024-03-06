d926ca5e2676a72b4cce80fabaa0748ea3f5b224
yongfong <yongfongzhang@163.com>
2024-03-06 17:54:53 +0800

    day44 programmatic navigation with navigationstack

--- 100DaysOfSwiftUI/Navigation/Navigation/ContentView.swift
+++ 100DaysOfSwiftUI/Navigation/Navigation/ContentView.swift
@@ -8,15 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var path = [Int]()
+    
     var body: some View {
-        NavigationStack {
-            List(0..<100) { i in
-                NavigationLink("Select \(i)", value: i)
+        NavigationStack(path: $path) {
+            VStack {
+                Button("show 32") {
+                    path = [32]
+                }
+                
+                Button("show 64") {
+                    path.append(64)
+                }
+                
+                Button("show 32 then 64") {
+                    path = [32, 64]
+                }
             }
             .navigationDestination(for: Int.self) { selection in
-                DetailView(number: selection)
+                Text("You selected \(selection)")
             }
         }
+//        NavigationStack {
+//            List(0..<100) { i in
+//                NavigationLink("Select \(i)", value: i)
+//            }
+//            .navigationDestination(for: Int.self) { selection in
+//                DetailView(number: selection)
+//            }
+//        }
     }
 }
 
