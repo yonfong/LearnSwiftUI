59e442a983edc2b1d15f5bc14b809229e842d6b9
yongfong <yongfongzhang@163.com>
2024-03-06 21:54:14 +0800

    day45 making your navigation title editable

--- 100DaysOfSwiftUI/Navigation/Navigation/ContentView.swift
+++ 100DaysOfSwiftUI/Navigation/Navigation/ContentView.swift
@@ -10,39 +10,50 @@ import SwiftUI
 struct ContentView: View {
     @State private var pathStore = PathStore()
     
+    @State private var title = "Swift UI"
+    
     var body: some View {
         NavigationStack {
-            List(0..<100) { index in
-                Text("Row \(index)")
-            }
-            .navigationTitle("Title goes here")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbarBackground(.blue)
-            .toolbarColorScheme(.dark)
-            .toolbar {
-//                ToolbarItem(placement: .topBarLeading) {
-//                    Button("Tap me") {
-//                        
-//                    }
-//                }
-//                
-//                ToolbarItem(placement: .topBarLeading) {
-//                    Button("Or Tap me") {
-//                        
-//                    }
-//                }
-                ToolbarItemGroup(placement: .topBarLeading) {
-                    Button("Tap me") {
-                    
-                    }
-                    
-                    Button("Or Tap me") {
-                    
-                    }
-                }
-            }
-//            .toolbar(.hidden, for: .navigationBar)
+            Text("hello world")
+                .navigationTitle($title)
+                .navigationBarTitleDisplayMode(.inline)
         }
+        
+        
+        
+        
+//        NavigationStack {
+//            List(0..<100) { index in
+//                Text("Row \(index)")
+//            }
+//            .navigationTitle("Title goes here")
+//            .navigationBarTitleDisplayMode(.inline)
+//            .toolbarBackground(.blue)
+//            .toolbarColorScheme(.dark)
+//            .toolbar {
+////                ToolbarItem(placement: .topBarLeading) {
+////                    Button("Tap me") {
+////                        
+////                    }
+////                }
+////                
+////                ToolbarItem(placement: .topBarLeading) {
+////                    Button("Or Tap me") {
+////                        
+////                    }
+////                }
+//                ToolbarItemGroup(placement: .topBarLeading) {
+//                    Button("Tap me") {
+//                    
+//                    }
+//                    
+//                    Button("Or Tap me") {
+//                    
+//                    }
+//                }
+//            }
+////            .toolbar(.hidden, for: .navigationBar)
+//        }
         
         
 //        NavigationStack(path: $pathStore.path) {
