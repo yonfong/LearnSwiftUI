0421192573d313f763ba617fb0eb425ca05c1d64
yongfong <yongfongzhang@163.com>
2024-03-11 10:37:32 +0800

    day50 adding haptic effects

--- 100DaysOfSwiftUI/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ 100DaysOfSwiftUI/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 struct ContentView: View {
     @State private var results = [Result]()
@@ -17,65 +18,111 @@ struct ContentView: View {
         return username.count < 5 || email.count < 5
     }
     
+    @State private var counter = 0
+    
+    @State private var engine: CHHapticEngine?
+    
     var body: some View {
-        NavigationStack {
-            Form {
-                Section {
-                    TextField("Username", text: $username)
-                    TextField("Email", text: $email)
-                }
-                
-                Section {
-                    Button("Create accont") {
-                        print("Creating account")
-                    }
-                }
-                .disabled(disableForm)
-            }
-            .toolbar {
-                Button("Create accont") {
-                    print("Creating account")
-                }
-                .disabled(username.isEmpty || email.isEmpty)
-            }
-        }
         
+//        Button("Tap count \(counter)") {
+//            counter += 1
+//        }
+//        .sensoryFeedback(.increase, trigger: counter)
         
-        List(results, id: \.trackId) { item in
-            HStack {
-//                AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
-//                    image
-//                     .resizable()
-//                     .scaledToFit()
-//                } placeholder: {
-////                    Color.red
-//                    ProgressView()
+        Button("Tap me", action: complexSuccess)
+            .onAppear(perform: {
+                prepareHaptics()
+            })
+        
+//        NavigationStack {
+//            Form {
+//                Section {
+//                    TextField("Username", text: $username)
+//                    TextField("Email", text: $email)
 //                }
-//                .frame(width:100, height:100)
-                AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
-                    if let image = phase.image {
-                        image
-                            .resizable()
-                            .scaledToFit()
-                    } else if phase.error != nil {
-                        Text("there was an error")
-                    } else {
-                        ProgressView()
-                    }
-                }
-                .frame(width: 100, height: 100)
-                VStack(alignment: .leading, content: {
-                    Text(item.trackName)
-                        .font(.headline)
-                    
-                    Text(item.collectionName)
-                })
-            }
-        }
-        .task {
-            await loadData()
+//                
+//                Section {
+//                    Button("Create accont") {
+//                        print("Creating account")
+//                    }
+//                }
+//                .disabled(disableForm)
+//            }
+//            .toolbar {
+//                Button("Create accont") {
+//                    print("Creating account")
+//                }
+//                .disabled(username.isEmpty || email.isEmpty)
+//            }
+//        }
+        
+        
+//        List(results, id: \.trackId) { item in
+//            HStack {
+////                AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
+////                    image
+////                     .resizable()
+////                     .scaledToFit()
+////                } placeholder: {
+//////                    Color.red
+////                    ProgressView()
+////                }
+////                .frame(width:100, height:100)
+//                AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
+//                    if let image = phase.image {
+//                        image
+//                            .resizable()
+//                            .scaledToFit()
+//                    } else if phase.error != nil {
+//                        Text("there was an error")
+//                    } else {
+//                        ProgressView()
+//                    }
+//                }
+//                .frame(width: 100, height: 100)
+//                VStack(alignment: .leading, content: {
+//                    Text(item.trackName)
+//                        .font(.headline)
+//                    
+//                    Text(item.collectionName)
+//                })
+//            }
+//        }
+//        .task {
+//            await loadData()
+//        }
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine")
         }
     }
+    
+    func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        var events = [CHHapticEvent]()
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+        
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription)")
+        }
+    }
+    
     
     func loadData() async {
         guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
