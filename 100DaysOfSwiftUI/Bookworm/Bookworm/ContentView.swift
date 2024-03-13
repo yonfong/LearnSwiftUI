7c93e417370be3fb70cc943793dcdb38141c41f0
yongfong <yongfongzhang@163.com>
2024-03-13 08:21:48 +0800

    day54 creating books with swiftData

--- 100DaysOfSwiftUI/Bookworm/Bookworm/ContentView.swift
+++ 100DaysOfSwiftUI/Bookworm/Bookworm/ContentView.swift
@@ -13,26 +13,44 @@ struct ContentView: View {
     
     @Query var students: [Student]
     @Environment(\.modelContext) var modelContext
+    @Query var books: [Book]
+    
+    @State private var showingAddScreen = false
     
     var body: some View {
         NavigationStack {
-            List(students) { student in
-                Text(student.name)
-            }
-            .navigationTitle("Classroom")
-            .toolbar {
-                Button("Add") {
-                    let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
-                    let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
-                    
-                    let chosenFirstName = firstNames.randomElement()!
-                    let chosenLastName = lastNames.randomElement()!
-                    
-                    let student = Student(id: UUID(), name: "\(chosenFirstName) \(chosenLastName)")
-                    modelContext.insert(student)
+            Text("Count: \(books.count)")
+                .navigationTitle("Bookworm")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Add Book", systemImage: "plus") {
+                            showingAddScreen.toggle()
+                        }
+                    }
                 }
-            }
+                .sheet(isPresented: $showingAddScreen, content: {
+                    AddBookView()
+                })
+            
         }
+            
+//            List(students) { student in
+//                Text(student.name)
+//            }
+//            .navigationTitle("Classroom")
+//            .toolbar {
+//                Button("Add") {
+//                    let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+//                    let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+//                    
+//                    let chosenFirstName = firstNames.randomElement()!
+//                    let chosenLastName = lastNames.randomElement()!
+//                    
+//                    let student = Student(id: UUID(), name: "\(chosenFirstName) \(chosenLastName)")
+//                    modelContext.insert(student)
+//                }
+//            }
+//        }
         
 //        VStack {
 //            PushButton(title: "Rembemr Me", isOn: $rememberMe)
