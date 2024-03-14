2251fd3e18552fd124c7f63a258da0990e647195
yongfong <yongfongzhang@163.com>
2024-03-14 10:02:19 +0800

    day57 dynamically sorting and filtering @Query with swiftUI

--- 100DaysOfSwiftUI/SwiftDataProject/SwiftDataProject/ContentView.swift
+++ 100DaysOfSwiftUI/SwiftDataProject/SwiftDataProject/ContentView.swift
@@ -10,41 +10,70 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query(filter: #Predicate<User> { user in
-        user.name.localizedStandardContains("R") && user.city == "London"
-    }, sort: \User.name) var users:[User]
+//    @Query(filter: #Predicate<User> { user in
+//        user.name.localizedStandardContains("R") && user.city == "London"
+//    }, sort: \User.name) var users:[User]
     @State private var path = [User]()
+    
+    @State private var showingUpcomingOnly = false
+    
+    @State private var sortOrder = [
+        SortDescriptor(\User.name),
+        SortDescriptor(\User.joinDate)
+    ]
     
     var body: some View {
         NavigationStack {
-            List(users) { user in
-                NavigationLink(value: user) {
-                    Text(user.name)
-                }
-            }
+//            List(users) { user in
+//                NavigationLink(value: user) {
+//                    Text(user.name)
+//                }
+//            }
+            UsersView(minimumJoinDate: showingUpcomingOnly ? .now : .distantPast, sortOrder: sortOrder)
             .navigationTitle("Users")
             .navigationDestination(for: User.self) { user in
                 EditUserView(user: user)
             }
             .toolbar {
-                Button("Add User", systemImage: "plus") {
-                    let user = User(name: "", city: "", joinDate: .now)
-                    modelContext.insert(user)
-                    path = [user]
+//                Button("Add User", systemImage: "plus") {
+//                    let user = User(name: "", city: "", joinDate: .now)
+//                    modelContext.insert(user)
+//                    path = [user]
+//                }
+                
+                Button(showingUpcomingOnly ? "Show Everyone" : "Show Upcoming") {
+                    showingUpcomingOnly.toggle()
                 }
                 
-                Button("Add Samples", systemImage: "plus") {
-                    try? modelContext.delete(model: User.self)
-                    let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
-                    let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
-                    let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
-                    let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
-
-                    modelContext.insert(first)
-                    modelContext.insert(second)
-                    modelContext.insert(third)
-                    modelContext.insert(fourth)
+                Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                    Picker("Sort", selection: $sortOrder) {
+                        Text("Sort By Name")
+                            .tag([
+                                SortDescriptor(\User.name),
+                                SortDescriptor(\User.joinDate)
+                            ])
+                        
+                        Text("Sort by Join Date")
+                            .tag([
+                                SortDescriptor(\User.joinDate),
+                                SortDescriptor(\User.name)
+                            ])
+                    }
                 }
+                
+                
+//                Button("Add Samples", systemImage: "plus") {
+//                    try? modelContext.delete(model: User.self)
+//                    let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
+//                    let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
+//                    let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
+//                    let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
+//
+//                    modelContext.insert(first)
+//                    modelContext.insert(second)
+//                    modelContext.insert(third)
+//                    modelContext.insert(fourth)
+//                }
             }
         }
     }
