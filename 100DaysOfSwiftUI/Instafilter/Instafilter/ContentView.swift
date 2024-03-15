3f7207b5701ce3e9247c56d3629b1e4b499e718c
yongfong <yongfongzhang@163.com>
2024-03-15 10:59:55 +0800

    day62 showing multiple options with confirmationDialog()

--- 100DaysOfSwiftUI/Instafilter/Instafilter/ContentView.swift
+++ 100DaysOfSwiftUI/Instafilter/Instafilter/ContentView.swift
@@ -9,11 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var blurAmount = 0.0 
-//    {
-////        didSet {
-////            print("New value is \(blurAmount)")
-////        }
-//    }
+
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
     
     var body: some View {
         VStack {
@@ -28,8 +26,23 @@ struct ContentView: View {
             Button("Random Blur") {
                 blurAmount = Double.random(in: 0...20)
             }
+            
+            Button("Hello world") {
+                showingConfirmation = true
+            }
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
         }
-        .padding()
+        .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+            Button("Red") { backgroundColor = .red }
+            Button("Green") { backgroundColor = .green }
+            Button("Blue") { backgroundColor = .blue }
+            Button("Cancel", role: .cancel) {  }
+            
+        } message: {
+            Text("Select a new color")
+        }
+
     }
 }
 
