e80e65d45e316299ad9f8a8d733b402db4d32ba9
yongfong <yongfongzhang@163.com>
2024-03-15 16:51:08 +0800

    day63 integrating core image with swiftui

--- 100DaysOfSwiftUI/Instafilter/Instafilter/ContentView.swift
+++ 100DaysOfSwiftUI/Instafilter/Instafilter/ContentView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
     @State private var blurAmount = 0.0 
@@ -13,36 +15,82 @@ struct ContentView: View {
     @State private var showingConfirmation = false
     @State private var backgroundColor = Color.white
     
+    @State private var image: Image?
+    
     var body: some View {
         VStack {
-            Text("Hello, world!")
-                .blur(radius: blurAmount)
-            
-            Slider(value: $blurAmount, in: 0...20)
-                .onChange(of: blurAmount) { oldValue, newValue in
-                    print("New value is \(newValue)")
-                }
-            
-            Button("Random Blur") {
-                blurAmount = Double.random(in: 0...20)
-            }
-            
-            Button("Hello world") {
-                showingConfirmation = true
-            }
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
+            image?
+                .resizable()
+                .scaledToFit()
+                .padding()
         }
-        .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-            Button("Red") { backgroundColor = .red }
-            Button("Green") { backgroundColor = .green }
-            Button("Blue") { backgroundColor = .blue }
-            Button("Cancel", role: .cancel) {  }
-            
-        } message: {
-            Text("Select a new color")
-        }
+        .onAppear(perform: loadImage)
+        
+        
+//        VStack {
+//            Text("Hello, world!")
+//                .blur(radius: blurAmount)
+//            
+//            Slider(value: $blurAmount, in: 0...20)
+//                .onChange(of: blurAmount) { oldValue, newValue in
+//                    print("New value is \(newValue)")
+//                }
+//            
+//            Button("Random Blur") {
+//                blurAmount = Double.random(in: 0...20)
+//            }
+//            
+//            Button("Hello world") {
+//                showingConfirmation = true
+//            }
+//            .frame(width: 100, height: 100)
+//            .background(backgroundColor)
+//        }
+//        .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+//            Button("Red") { backgroundColor = .red }
+//            Button("Green") { backgroundColor = .green }
+//            Button("Blue") { backgroundColor = .blue }
+//            Button("Cancel", role: .cancel) {  }
+//            
+//        } message: {
+//            Text("Select a new color")
+//        }
 
+    }
+    
+    func loadImage() {
+        let inputImage = UIImage(resource: .test)
+        let beginImage = CIImage(image: inputImage)
+//        image = Image(.test)
+        
+        let context = CIContext()
+//        let currentFilter = CIFilter.sepiaTone()
+//        currentFilter.inputImage = beginImage
+//        currentFilter.intensity = 1
+//        
+//        let currentFilter = CIFilter.pixellate()
+//        currentFilter.inputImage = beginImage
+//        currentFilter.scale = 5
+        
+//        let currentFilter = CIFilter.crystallize()
+//        currentFilter.inputImage = beginImage
+//        currentFilter.radius = 10
+        
+        let currentFilter = CIFilter.twirlDistortion()
+        currentFilter.inputImage = beginImage
+        let amount = 1.0
+
+        let inputKeys = currentFilter.inputKeys
+
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 100, forKey: kCIInputScaleKey) }
+        
+        guard let outImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outImage, from: outImage.extent) else { return  }
+        let uiImage = UIImage(cgImage: cgImage)
+        image = Image(uiImage: uiImage)
     }
 }
 
