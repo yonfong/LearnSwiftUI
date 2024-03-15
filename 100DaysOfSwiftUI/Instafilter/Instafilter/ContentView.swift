5af3adb38a765c1b9920886c57e715ed1dd9c3b8
yongfong <yongfongzhang@163.com>
2024-03-15 10:46:00 +0800

    day62 how property wrappers become structs

---
+++ 100DaysOfSwiftUI/Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by sky on 2024/3/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var blurAmount = 0.0 {
+        didSet {
+            print("New value is \(blurAmount)")
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .blur(radius: blurAmount)
+            
+            Slider(value: $blurAmount, in: 0...20)
+            
+            Button("Random Blur") {
+                blurAmount = Double.random(in: 0...20)
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 100DaysOfSwiftUI/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by sky on 2024/3/15.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
