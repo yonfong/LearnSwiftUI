a77c3a5e6a1395c330a25f25f8ae4dc6b6a7331e
yongfong <yongfongzhang@163.com>
2024-03-17 21:03:42 +0800

    day74 identifying views with useful labels

---
+++ 100DaysOfSwiftUI/AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by sky on 2024/3/17.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 100DaysOfSwiftUI/AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  AccessibilitySandbox
+//
+//  Created by sky on 2024/3/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks",
+    ]
+
+    
+    @State private var selectedPicture = Int.random(in: 0...3)
+    
+    var body: some View {
+        VStack {
+            Image(pictures[selectedPicture])
+                .resizable()
+                .scaledToFit()
+                .onTapGesture {
+                    selectedPicture = Int.random(in: 0...3)
+                }
+                .accessibilityLabel(labels[selectedPicture])
+                .accessibilityRemoveTraits(.isButton)
+            
+            Button {
+                selectedPicture = Int.random(in: 0...3)
+            } label: {
+                Image(pictures[selectedPicture])
+                    .resizable()
+                    .scaledToFit()
+            }
+            .accessibilityLabel(labels[selectedPicture])
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
