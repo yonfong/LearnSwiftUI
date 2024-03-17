946343ede25f9ec126a3f581018620457df56980
yongfong <yongfongzhang@163.com>
2024-03-17 11:44:47 +0800

    day70 selecting and editing map annotations

--- 100DaysOfSwiftUI/BucketList/BucketList/ContentView.swift
+++ 100DaysOfSwiftUI/BucketList/BucketList/ContentView.swift
@@ -64,6 +64,8 @@ struct ContentView: View {
     
     @State private var locations = [Location]()
     
+    @State private var selectedPlace: Location?
+    
     var body: some View {
         MapReader { proxy in
             Map(initialPosition: startPosition) {
@@ -77,6 +79,9 @@ struct ContentView: View {
                             .frame(width: 44, height: 44)
                             .background(.white)
                             .clipShape(.circle)
+                            .onLongPressGesture {
+                                selectedPlace = location
+                            }
                     }
                     
                 }
@@ -89,6 +94,13 @@ struct ContentView: View {
                         locations.append(newLocation)
                     }
                 }
+        }
+        .sheet(item: $selectedPlace) { place in
+            EditView(location: place) { newLocation in
+                if let index = locations.firstIndex(of: place) {
+                    locations[index] = newLocation
+                }
+            }
         }
         
         
