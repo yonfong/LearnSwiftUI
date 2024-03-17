1cb629caa2ab1a7cfffcc93c72a79635118bd073
yongfong <yongfongzhang@163.com>
2024-03-17 10:13:51 +0800

    integrating mapkit with swiftUI

--- 100DaysOfSwiftUI/BucketList/BucketList/ContentView.swift
+++ 100DaysOfSwiftUI/BucketList/BucketList/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import MapKit
 
 enum LoadingState {
     case loading, success, failed
@@ -29,20 +30,88 @@ struct FailedView: View {
     }
 }
 
+struct Location: Identifiable {
+    let id = UUID()
+    var name: String
+    var coordinate: CLLocationCoordinate2D
+}
 
 struct ContentView: View {
     @State private var loadingState = LoadingState.loading
     
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
+    
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    
     var body: some View {
-        switch loadingState {
-        case .loading:
-            LoadingView()
-        case .success:
-            SuccessView()
-        case .failed:
-            FailedView()
-        }
+//        switch loadingState {
+//        case .loading:
+//            LoadingView()
+//        case .success:
+//            SuccessView()
+//        case .failed:
+//            FailedView()
+//        }
         
+        VStack {
+            MapReader { proxy in
+                Map()
+                    .onTapGesture { position in
+                        if let coordinate = proxy.convert(position, from: .local) {
+                            print(coordinate)
+                        }
+                    }
+            }
+            
+            Map(position: $position) {
+                ForEach(locations) { location in
+//                    Marker(location.name, coordinate: location.coordinate)
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Text(location.name)
+                            .font(.headline)
+                            .padding()
+                            .background(.blue)
+                            .foregroundStyle(.white)
+                            .clipShape(.capsule)
+                    }
+                    .annotationTitles(.hidden)
+                }
+            }
+                .onMapCameraChange { context in
+                    print(context.region)
+                }
+                .onMapCameraChange(frequency: .continuous) { context in
+                    print(context.region)
+                }
+            
+            HStack(spacing: 50) {
+                Button("Paris") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                        )
+                    )
+                }
+
+                Button("Tokyo") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                        )
+                    )
+                }
+            }
+        }
         
         
 //        Button("Read and write") {
