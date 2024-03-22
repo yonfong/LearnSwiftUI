24db7df27889ee04cda3a1332bc59df6beb8e25f
yongfong <yongfongzhang@163.com>
2024-03-22 11:34:20 +0800

    day80 Understanding Swift’s Result type

--- 100DaysOfSwiftUI/HotProspects/HotProspects/ContentView.swift
+++ 100DaysOfSwiftUI/HotProspects/HotProspects/ContentView.swift
@@ -16,26 +16,35 @@ struct ContentView: View {
     
     @State private var selection = Set<String>()
     
+    @State private var output = ""
+    
     var body: some View {
-        TabView(selection: $selectedTab) {
-            VStack {
-                Text("Tab 1")
-                
-                Button("Show tab 2") {
-                    selectedTab = "Two"
-                }
+        
+        Text(output)
+            .task {
+                await fetchReadings()
             }
-            .tabItem {
-                Label("One", systemImage: "star")
-            }
-            .tag("One")
-            
-            Text("Tab 2")
-                .tabItem {
-                    Label("Two", systemImage: "circle")
-                }
-                .tag("Two")
-        }
+        
+        
+//        TabView(selection: $selectedTab) {
+//            VStack {
+//                Text("Tab 1")
+//                
+//                Button("Show tab 2") {
+//                    selectedTab = "Two"
+//                }
+//            }
+//            .tabItem {
+//                Label("One", systemImage: "star")
+//            }
+//            .tag("One")
+//            
+//            Text("Tab 2")
+//                .tabItem {
+//                    Label("Two", systemImage: "circle")
+//                }
+//                .tag("Two")
+//        }
         
 //        List(users, id: \.self, selection: $selection) { user in
 //            Text(user)
@@ -45,6 +54,36 @@ struct ContentView: View {
 //            Text("You slected \(selection.formatted())")
 //        }
 //        EditButton()
+    }
+    
+    func fetchReadings() async {
+//        do {
+//            let url = URL(string: "https://hws.dev/readings.json")!
+//            let (data, _) = try await URLSession.shared.data(from: url)
+//            
+//            let readings = try JSONDecoder().decode([Double].self, from: data)
+//            
+//            output = "found \(readings.count) readings"
+//        } catch {
+//            print("Download error")
+//        }
+        
+        let fetchTask = Task {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+
+            return "found \(readings.count) readings"
+        }
+        
+        let result = await fetchTask.result
+        
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let error):
+            output = "Error: \(error.localizedDescription)"
+        }
     }
 }
 
