80d7b79fbaef84b94ca672ab1e8e0f797864b806
yongfong <yongfongzhang@163.com>
2024-03-23 20:11:04 +0800

    day84: Adding a context menu to an image

--- 100DaysOfSwiftUI/HotProspects/HotProspects/MeView.swift
+++ 100DaysOfSwiftUI/HotProspects/HotProspects/MeView.swift
@@ -15,6 +15,8 @@ struct MeView: View {
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
     
+    @State private var qrCode = UIImage()
+    
     var body: some View {
         NavigationStack {
             Form {
@@ -26,13 +28,19 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrCode)
                     .interpolation(.none)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        ShareLink(item: Image(uiImage: qrCode), preview: SharePreview("My QR Code", image: Image(uiImage: qrCode)))
+                    }
             }
             .navigationTitle("Your code")
+            .onAppear(perform: updateCode)
+            .onChange(of: name, updateCode)
+            .onChange(of: emailAddress, updateCode)
         }
     }
     
@@ -46,6 +54,10 @@ struct MeView: View {
         }
         
         return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+    
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
     }
 }
 
