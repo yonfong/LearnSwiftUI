6d203c84d8feb871f4b28a06e22308ff30a50e19
yongfong <yongfongzhang@163.com>
2024-03-25 15:56:01 +0800

    day93: ScrollView effects using GeometryReader

--- 100DaysOfSwiftUI/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ 100DaysOfSwiftUI/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -11,7 +11,10 @@ struct ContentView: View {
     var body: some View {
 //        CustomAlignmentGuideView()
 //        AbsolutePositioningView()
-        GeometryReaderView()
+//        GeometryReaderView()
+//        ScrollEffectView()
+        
+        CoverFlowView()
         
 //        VStack {
 //            Image(systemName: "globe")
@@ -85,6 +88,50 @@ struct InnerView: View {
             }
             .background(.orange)
             Text("Right")
+        }
+    }
+}
+
+struct CoverFlowView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { num in
+                    GeometryReader { proxy in
+                        Text("Number \(num)")
+                            .font(.largeTitle)
+                            .padding()
+                            .background(.red)
+                            .rotation3DEffect(.degrees(-proxy.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
+                            .frame(width: 200, height: 200)
+                    }
+                    .frame(width: 200, height: 200)
+                }
+            }
+        }
+    }
+}
+
+struct ScrollEffectView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    var body: some View {
+        GeometryReader { fullView in
+            ScrollView {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(
+                                .degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5,
+                                axis: (x: 0.0, y: 1.0, z: 0.0)
+                            )
+                    }
+                    .frame(height: 40)
+                }
+            }
+            
         }
     }
 }
