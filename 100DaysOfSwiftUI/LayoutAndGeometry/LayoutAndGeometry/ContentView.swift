d5a576d7a3b8ac6ab3c394f4abc7e9cdf58ec911
yongfong <yongfongzhang@163.com>
2024-03-25 11:58:33 +0800

    day92: How to create a custom alignment guide

--- 100DaysOfSwiftUI/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ 100DaysOfSwiftUI/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,49 +9,87 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-                .padding(20)
-                .background(.red)
-        }
-        .padding()
+        CustomAlignmentGuideView()
         
-        HStack(alignment: .lastTextBaseline) {
-            Text("Live")
-                .font(.caption)
-            Text("long")
-            Text("and")
-                .font(.title)
-            Text("prosper")
-                .font(.largeTitle)
-        }
         
-        VStack(alignment: .leading) {
-            Text("holly wow")
-                .alignmentGuide(.leading, computeValue: { dimension in
-                    dimension[.trailing]
-                })
-            Text("this is a long line of text")
-        }
-        .background(.red)
-        .frame(width: 400, height: 400)
-        .background(.blue)
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
+//                .padding(20)
+//                .background(.red)
+//        }
+//        .padding()
         
-        VStack(alignment: .leading) {
-                ForEach(0..<10) { position in
-                    Text("Number \(position)")
-                        .alignmentGuide(.leading) { _ in Double(position) * -10 }
-                }
-            }
-            .background(.red)
-            .frame(width: 400, height: 400)
-            .background(.blue)
+//        HStack(alignment: .lastTextBaseline) {
+//            Text("Live")
+//                .font(.caption)
+//            Text("long")
+//            Text("and")
+//                .font(.title)
+//            Text("prosper")
+//                .font(.largeTitle)
+//        }
+//        
+//        VStack(alignment: .leading) {
+//            Text("holly wow")
+//                .alignmentGuide(.leading, computeValue: { dimension in
+//                    dimension[.trailing]
+//                })
+//            Text("this is a long line of text")
+//        }
+//        .background(.red)
+//        .frame(width: 400, height: 400)
+//        .background(.blue)
+//        
+//        VStack(alignment: .leading) {
+//                ForEach(0..<10) { position in
+//                    Text("Number \(position)")
+//                        .alignmentGuide(.leading) { _ in Double(position) * -10 }
+//                }
+//            }
+//            .background(.red)
+//            .frame(width: 400, height: 400)
+//            .background(.blue)
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct CustomAlignmentGuideView: View {
+    var body: some View {
+        HStack(alignment: .midAcountAndName) {
+            VStack {
+                Text("@twostraws")
+                    .alignmentGuide(.midAcountAndName, computeValue: { dimension in
+                        dimension[VerticalAlignment.center]
+                    })
+                Image(.example)
+                    .resizable()
+                    .frame(width: 64, height: 64)
+            }
+            
+            VStack {
+                Text("FULL NAME")
+                Text("PAUL HUDSON")
+                    .alignmentGuide(.midAcountAndName, computeValue: { dimension in
+                        dimension[VerticalAlignment.center]
+                    })
+                    .font(.largeTitle)
+            }
+        }
+    }
+}
+
+extension VerticalAlignment {
+    struct MidAccountAndName: AlignmentID {
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            context[.top]
+        }
+    }
+    
+    static let midAcountAndName = VerticalAlignment(MidAccountAndName.self)
 }
