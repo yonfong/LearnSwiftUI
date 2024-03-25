a72bbe8d62116d8ed05641fe4b6290a1d81ef662
yongfong <yongfongzhang@163.com>
2024-03-25 16:05:10 +0800

    day93: ScrollView effects using visualEffect() and scrollTargetBehavior()

--- 100DaysOfSwiftUI/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ 100DaysOfSwiftUI/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -97,18 +97,36 @@ struct CoverFlowView: View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(spacing: 0) {
                 ForEach(1..<20) { num in
-                    GeometryReader { proxy in
-                        Text("Number \(num)")
-                            .font(.largeTitle)
-                            .padding()
-                            .background(.red)
-                            .rotation3DEffect(.degrees(-proxy.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
-                            .frame(width: 200, height: 200)
-                    }
-                    .frame(width: 200, height: 200)
+                    Text("Number \(num)")
+                        .font(.largeTitle)
+                        .padding()
+                        .background(.red)
+                        .frame(width: 200, height: 200)
+                        .visualEffect { content, geometryProxy in
+                            content.rotation3DEffect(.degrees(-geometryProxy.frame(in: .global).minX / 8), axis: (x: 0, y: 1, z: 0))
+                        }
                 }
             }
+            .scrollTargetLayout()
         }
+        .scrollTargetBehavior(.viewAligned)
+        
+        
+//        ScrollView(.horizontal, showsIndicators: false) {
+//            HStack(spacing: 0) {
+//                ForEach(1..<20) { num in
+//                    GeometryReader { proxy in
+//                        Text("Number \(num)")
+//                            .font(.largeTitle)
+//                            .padding()
+//                            .background(.red)
+//                            .rotation3DEffect(.degrees(-proxy.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
+//                            .frame(width: 200, height: 200)
+//                    }
+//                    .frame(width: 200, height: 200)
+//                }
+//            }
+//        }
     }
 }
 
